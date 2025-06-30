5320604208286cecdbf6926909a7383f453178d3
David0414 <david.mendoza.perez74@gmail.com>
2025-06-30 15:26:54 -0600

    Zpreuba 11

--- src/components/models/hero_models/HeroExperience.jsx
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -15,20 +15,23 @@ const HeroExperience = () => {
   const MemoizedRoom = useMemo(() => <Room />, []);
 
   return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+    <Canvas
+      dpr={isMobile ? 1 : window.devicePixelRatio}
+      camera={{ position: [0, 0, 15], fov: 45 }}
+      gl={{ preserveDrawingBuffer: false, powerPreference: "low-power" }}
+    >
       <ambientLight intensity={0.2} color="#1a1a40" />
       <OrbitControls
-        enablePan={false} 
-        enableZoom={!isTablet} 
+        enablePan={false}
+        enableZoom={!isTablet}
         maxDistance={20}
         minDistance={5}
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
       />
-      
-      <Suspense fallback={null}>
-        <HeroLights />
-        <Particles count={50} /> {/* Reducción de partículas */}
+      <Suspense fallback={<span className="text-white">Cargando modelo...</span>}>
+        {!isMobile && <HeroLights />}
+        <Particles count={isMobile ? 20 : 50} />
         <group
           scale={isMobile ? 0.7 : 1}
           position={[0, -3.5, 0]}
@@ -38,6 +41,7 @@ const HeroExperience = () => {
         </group>
       </Suspense>
     </Canvas>
+
   );
 };
 
